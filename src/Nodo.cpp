a1eabf6b7461d88194b399eb5aa5d72dbed2b907
rodrigo <rmayett@comunidad.unam.mx>
2016-08-29 21:30:31 -0500

    version final  del proyecto

--- src/Nodo.cpp
+++ src/Nodo.cpp
@@ -1,32 +1,36 @@
+/*Rodrigo Mayett Guzman
+el programa define los metodos 
+del nodo que fueron definidos en Nodo.h*/
 #include "Nodo.h"
-
+//crea un nodo con un dato dado por 
+//el usuario y establece el siguiente 
 Nodo::Nodo(int Dato , Nodo* Sig)
 {
 	this->Dato = Dato;
 	this->Sig = Sig;
 }
-
+//crea un nodo con un dato unico
 Nodo::Nodo(int Dato)
 {
 	this->Dato = Dato;
 	this->Sig = NULL;
 }
-
+//cambia el dato guardado en el nodo
 void Nodo::setDato(int Dato)
 {
 	this->Dato = Dato;
 }
-
+//establece cual sera el siguiente nodo del actual
 void Nodo::setSig(Nodo* Sig)
 {
 	this->Sig = Sig;
 }
-
+//obtiene el dato guardado en el nodo
 int Nodo::getDato()
 {
 	return this->Dato;
 }
-
+//obtiene el nodo siguiente del actual
 Nodo* Nodo::getSig()
 {
 	return this->Sig;
